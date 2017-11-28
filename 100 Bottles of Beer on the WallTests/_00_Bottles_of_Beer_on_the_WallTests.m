ead3efd9a39d82cc8877cc8eec928d9e276b5fd1
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2017-11-28 13:59:48 -0400

    Initial Commit

---
+++ 100 Bottles of Beer on the WallTests/_00_Bottles_of_Beer_on_the_WallTests.m
@@ -0,0 +1,39 @@
+//
+//  _00_Bottles_of_Beer_on_the_WallTests.m
+//  100 Bottles of Beer on the WallTests
+//
+//  Created by Eduardo de la Cruz on 28/11/17.
+//  Copyright © 2017 Eduardo de la Cruz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _00_Bottles_of_Beer_on_the_WallTests : XCTestCase
+
+@end
+
+@implementation _00_Bottles_of_Beer_on_the_WallTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
