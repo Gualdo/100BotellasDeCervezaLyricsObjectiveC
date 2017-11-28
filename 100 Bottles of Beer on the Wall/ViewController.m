71b09fdfe19767c2231d6623fd22d2cfdaf4bc0c
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2017-11-28 14:44:17 -0400

    All done

--- 100 Bottles of Beer on the Wall/ViewController.m
+++ 100 Bottles of Beer on the Wall/ViewController.m
@@ -14,16 +14,34 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    int numberOfBottles = 100;
+    
+    for (int i = 0; i <= numberOfBottles; i++)
+    {
+        if (i < numberOfBottles - 1)
+        {
+            NSLog(@"\n\n%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.", numberOfBottles - i, numberOfBottles - i, (numberOfBottles - (i + 1)));
+        }
+        else if (i == numberOfBottles - 1)
+        {
+            NSLog(@"\n\n%d bottle of beer on the wall, %d bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.", numberOfBottles - i, numberOfBottles - i);
+        }
+        else
+        {
+            NSLog(@"\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, %d bottles of beer on the wall.", numberOfBottles);
+        }
+    }
 }
 
-
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
